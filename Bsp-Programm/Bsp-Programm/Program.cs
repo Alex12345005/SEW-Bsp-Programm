60d4c362aa8f2695dbade62fb51f10651f8e676d
Alexander Rath-Kienreich <rath190134@sr.htlweiz.at>
2020-04-04 14:13:14 +0200

    Fertig

---
+++ Bsp-Programm/Bsp-Programm/Program.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bsp_Programm
+{
+    class SLinkList
+
+    {
+
+        private int data;
+
+        private SLinkList next;
+
+
+
+        public SLinkList()
+
+        {
+
+            data = 0;
+
+            next = null;
+
+        }
+
+
+
+        public SLinkList(int value)
+
+        {
+
+            data = value;
+
+            next = null;
+
+        }
+
+
+
+
+
+        public SLinkList InsertNext(int value)
+
+        {
+
+            SLinkList node = new SLinkList(value);
+
+            if (this.next == null)
+
+            {
+
+                // Easy to handle
+
+                node.next = null; // already set in constructor
+
+                this.next = node;
+
+            }
+
+            else
+
+            {
+
+                // Insert in the middle
+
+                SLinkList temp = this.next;
+
+                node.next = temp;
+
+                this.next = node;
+
+            }
+
+            return node;
+
+        }
+
+
+
+        public int DeleteNext()
+
+        {
+
+            if (next == null)
+
+                return 0;
+
+
+
+            SLinkList node = this.next;
+
+            this.next = this.next.next;  // can be NULL here
+
+            node = null;
+
+            return 1;
+
+        }
+
+
+
+        public void Traverse(SLinkList node)
+
+        {
+
+            if (node == null)
+
+                node = this;
+
+            System.Console.WriteLine("\n\ncontinue and delete\n\n");
+
+
+
+            while (node != null)
+
+            {
+
+                System.Console.WriteLine(node.data);
+
+                node = node.next;
+
+            }
+
+        }
+
+
+
+    }
+
+
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            Console.WriteLine("Bsp.- Programm");
+            Console.WriteLine("_______________________________________");
+
+
+            SLinkList node1 = new SLinkList(1);
+
+            SLinkList node2 = node1.InsertNext(2);
+
+            SLinkList node3 = node2.InsertNext(3);
+
+            SLinkList node4 = node3.InsertNext(4);
+
+            SLinkList node5 = node4.InsertNext(5);
+
+            SLinkList node6 = node5.InsertNext(6);
+
+            SLinkList node7 = node6.InsertNext(7);
+
+            SLinkList node8 = node7.InsertNext(8);
+
+            SLinkList node9 = node8.InsertNext(9);
+
+            SLinkList node10 = node9.InsertNext(10);
+
+            SLinkList node11 = node10.InsertNext(11);
+
+            SLinkList node12 = node11.InsertNext(12);
+
+            SLinkList node13 = node12.InsertNext(13);
+
+            SLinkList node14 = node13.InsertNext(14);
+
+            SLinkList node15 = node14.InsertNext(15);
+
+            SLinkList node16 = node15.InsertNext(16);
+
+            SLinkList node17 = node16.InsertNext(17);
+
+            SLinkList node18 = node17.InsertNext(18);
+
+            SLinkList node19 = node18.InsertNext(19);
+
+            SLinkList node20 = node19.InsertNext(20);
+
+            Console.WriteLine("______________________________________________");
+
+            node1.Traverse(null);
+
+            node3.DeleteNext(); // delete the node "4"
+
+            Console.WriteLine("______________________________________________");
+
+            node2.Traverse(null);
+
+            node8.DeleteNext(); //delete the node "9"
+
+            Console.WriteLine("______________________________________________");
+
+            node3.Traverse(null);
+
+            node19.DeleteNext();
+
+            node13.DeleteNext();
+
+            Console.WriteLine("______________________________________________");
+
+            node11.Traverse(null);
+
+            node16.DeleteNext();
+
+
+
+        }
+
+    }
+
+
+}
---
+++ Bsp-Programm/Bsp-Programm/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("Bsp-Programm")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Bsp-Programm")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("788b3d14-0bc1-49a2-be9c-23e4e2e72a50")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// indem Sie "*" wie unten gezeigt eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
